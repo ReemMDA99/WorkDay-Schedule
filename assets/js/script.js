82ff7746072a1c15b9e9c17dc58df8f2eb7eb709
Reem Ahmed <reem.mda0909@gmail.com>
2022-04-04 11:44:58 -0600

    create task and event functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,32 @@
-//Estimate working hours
-//Create variables to store current date and time using moment.js
-//Create variables to show current date and time using moment.js
-// Create a variable to local storage the data
-//Create variable to save and delete functions
-//add event listener to call the save and delete upon click
+//Estimate working hours 9am-5pm
+var timeBlock = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 
+// create a variable to show present past and future tasks
+var taskContainerEl =document.querySelector(".container");
+var eventEl = document.querySelector(".event");
+
+//Create variables to store current date and time using moment.js
+var currentDayEl = document.querySelector("#currentDay");
+var currentHour = (moment().format);
+console.log(currentHour);
+
+//Create variables to show current date and time using moment.js
+var showDay = function () {
+var today = moment().format("dddd MMMM Do  YYYY, h a");
+currentDayEl.innerHTML = today;
+};
+showDay();
+
+//Display current time
+var showTime = function () {
+    for (var counter=0; counter < timeBlock.length; counter++) {
+        if (currentHour > timeBlock[counter]) {
+
+        }
+    }
+}
+// Create a variable to local storage the data
+
+//Create variable to save and delete functions
+
+//add event listener to call the save and delete upon click
