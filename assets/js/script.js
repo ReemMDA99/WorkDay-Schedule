bb41f1153655a4a84d289a52ed3235ffea1a585a
Reem Ahmed <reem.mda0909@gmail.com>
2022-04-04 10:17:30 -0600

    create pseudo code in js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//Estimate working hours
+//Create variables to store current date and time using moment.js
+//Create variables to show current date and time using moment.js
+// Create a variable to local storage the data
+//Create variable to save and delete functions
+//add event listener to call the save and delete upon click
+
