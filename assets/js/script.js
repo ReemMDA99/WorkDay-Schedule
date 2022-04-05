f1e2d4486429a908c6cb3d4773d9151458968409
Reem Ahmed <reem.mda0909@gmail.com>
2022-04-05 05:22:48 -0600

    errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
+var currentDayEl = document.querySelector("#currentDay");
+
 //Estimate working hours 9am-5pm
 var timeBlock = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 
 // create a variable to show present past and future tasks
 var taskContainerEl =document.querySelector(".container");
-var eventEl = document.querySelector(".event");
+var eventsEl = document.querySelectorAll(".events");
 
 //Create variables to store current date and time using moment.js
-var currentDayEl = document.querySelector("#currentDay");
 var currentHour = (moment().format('LLLL'));
-console.log(currentHour);
+
 
 //Create variables to show current date and time using moment.js
 var showDay = function () {
@@ -19,28 +20,45 @@ showDay();
 
 //Display current time
 var showTime = function() {
-    for (var counter=0; counter < timeBlock.length; counter++) {
+    for (var counter = 0; counter < timeBlock.length; counter++) {
         if (currentHour > timeBlock[counter]) {
-        eventEl[counter].classList.add("past");
+        eventsEl[counter].classList.add("past");
         } else if  (currentHour < timeBlock[counter]) {
-            eventEl[counter].classList.add("future");
+            eventsEl[counter].classList.add("future");
         } else {
-            eventEl[counter].classList.add("present");
+            eventsEl[counter].classList.add("present");
         }
     }
 };
-console.log(showTime);
-// Create a variable to local storage the data
+    
+showTime();
+
+// Create a variable to set item in local storage
 var storeEvents = function(event) {
-var hour = event.target.parentNode.children[0].textContent;
+var hours = event.target.parentNode.children[0].textContent;
 
 var events = event.target.parentNode.children[1].value;
-localStorage.setItem(hour, events);
-
+localStorage.setItem(hours, events);
 };
+// Create a variable to get item from local storage
 
+var getTask = function() {
+    for(var counter = 0; counter < eventsEl.length ; counter++) {
+        var hours = eventsEl[counter].parentNode.children[0].textContent;
+    var storeValue = localStorage.getItem(hours);
+    eventsEl[counter].textContent = storeValue;
+    }
+};
+getTask();
 
 //Create variable to save and delete functions
 var storeBtnEl = document.querySelectorAll(".storeBtn");
 var deleteBtnEl = document.querySelectorAll(".deleteBtn");
+
 //add event listener to call the save and delete upon click
+var clickEvents = function() {
+    for (var counter = 0; counter < storeBtnEl.length; counter++) {
+    storeBtnEl[counter].addEventListener("click", storeEvents);
+    }
+};
+clickEvents();
